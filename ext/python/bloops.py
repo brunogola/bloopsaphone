3fd316e2ccac9e2cd7ce115d0104d3de55df5282
Bruno Gola <brunogola@gmail.com>
2010-10-27 19:43:46 -0200

    all api prototyped

--- ext/python/bloops.py
+++ ext/python/bloops.py
@@ -175,23 +175,58 @@ c_bloopsmix._fields_ = [
 ## prototypes
 
 # bloops *bloops_new();
+# void bloops_ref(bloops *);
+# void bloops_destroy(bloops *);
 lib.bloops_new.restype = POINTER(c_bloops)
+lib.bloops_ref.argtypes = [POINTER(c_bloops)]
+lib.bloops_destroy.argtypes = [POINTER(c_bloops)]
 
-# bloopsaphone *bloops_square();
-lib.bloops_square.restype = POINTER(c_bloopsaphone)
+# void bloops_clear(bloops *);
+# void bloops_tempo(bloops *, int tempo);
+# void bloops_play(bloops *);
+# void bloops_stop(bloops *);
+# int bloops_is_done(bloops *);
+lib.bloops_clear.argtypes = [POINTER(c_bloops)]
+lib.bloops_tempo.argtypes = [POINTER(c_bloops), c_int]
+lib.bloops_play.argtypes = [POINTER(c_bloops)]
+lib.bloops_stop.argtypes = [POINTER(c_bloops)]
+lib.bloops_is_done.argtypes = [POINTER(c_bloops)]
 
+# bloopsatrack *bloops_track(bloops *, bloopsaphone *, char *, int);
 # bloopsatrack *bloops_track2(bloops *, bloopsaphone *, char *);
+# void bloops_track_ref(bloopsatrack *);
+# void bloops_track_destroy(bloopsatrack *);
+lib.bloops_track.restype = POINTER(c_bloopsatrack)
+lib.bloops_track.argtypes = [POINTER(c_bloops), POINTER(c_bloopsaphone), c_char_p, c_int]
 lib.bloops_track2.restype = POINTER(c_bloopsatrack)
 lib.bloops_track2.argtypes = [POINTER(c_bloops), POINTER(c_bloopsaphone), c_char_p]
+lib.bloops_track_ref.argtypes = [POINTER(c_bloopsatrack)]
+lib.bloops_track_destroy.argtypes = [POINTER(c_bloopsatrack)]
 
+# bloopsaphone *bloops_square();
+# bloopsaphone *bloops_sound_file(bloops *, char *);
+# void bloops_sound_copy(bloopsaphone *, bloopsaphone const *);
+# void bloops_sound_ref(bloopsaphone *);
 # void bloops_sound_destroy(bloopsaphone *);
+lib.bloops_square.restype = POINTER(c_bloopsaphone)
+lib.bloops_sound_file.restype = POINTER(c_bloopsaphone)
+lib.bloops_sound_file.argtypes = [POINTER(c_bloopsaphone), c_char_p]
+lib.bloops_sound_copy.argtypes = [POINTER(c_bloopsaphone), POINTER(c_bloopsaphone)]
+lib.bloops_sound_ref.argtypes = [POINTER(c_bloopsaphone)]
 lib.bloops_sound_destroy.argtypes = [POINTER(c_bloopsaphone)]
 
-# void bloops_play(bloops *);
-lib.bloops_play.argtypes = [POINTER(c_bloops)]
-
-# int bloops_is_done(bloops *);
-lib.bloops_is_done.argtypes = [POINTER(c_bloops)]
+# char *bloops_track_str(bloopsatrack *);
+# char *bloops_fxcmd_name(bloopsafxcmd fxcmd);
+# float bloops_note_freq(char, int);
+# char *bloops_sound_str(bloopsaphone *);
+lib.bloops_track_str.argtypes = [POINTER(c_bloopsatrack)]
+lib.bloops_track_str.restype = c_char_p
+lib.bloops_fxcmd_name.argtypes = [POINTER(c_bloopsafxcmd)]
+lib.bloops_fxcmd_name.restype = c_char_p
+lib.bloops_note_freq.argtypes = [c_char, c_int]
+lib.bloops_note_freq.restype = c_float
+lib.bloops_sound_str.argtypes = [POINTER(c_bloopsaphone)]
+lib.bloops_sound_str.restype = c_char_p (bloopsaphone *)
 
 def test():
     B = lib.bloops_new()
